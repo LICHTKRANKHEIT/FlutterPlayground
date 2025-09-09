253855ee8516a7bd996ff64852585cdf5ab4818b
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2025-09-09 23:05:42 +0900

    commit Test

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        home: Scaffold(
-            
-        )
+      debugShowCheckedModeBanner: false,
+      home: MyStatefulPage(),
     );
+  }
+}
 
+class MyStatefulPage extends StatefulWidget {
+  const MyStatefulPage({super.key});
+
+  @override
+  State<MyStatefulPage> createState() => _MyStatefulPageState();
+}
+
+
+class _MyStatefulPageState extends State<MyStatefulPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
   }
 }
