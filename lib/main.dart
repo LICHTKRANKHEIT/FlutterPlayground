21c7525083f4822bfc0372d602d202abeef619d8
李华 <lihua628.biz@gmail.com>
2025-09-10 21:18:44 +0900

    창 띄우기 (showDialog, AlertDialog)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,7 +30,33 @@ class _MyStatefulPageState extends State<MyStatefulPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: IconButton(onPressed: () {
+          showDialog(context: context, builder: (BuildContext context) {
+            return AlertDialog(
+              content: Container(
+                width: 250,
+                height: 100,
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        Text("알림"),
+                        Text("이거 알림임 ㅅㄱ")
+                      ],
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: IconButton(onPressed: () {Navigator.of(context).pop();}, icon: Icon(Icons.close)),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },);
+        }, icon: Icon(Icons.info)),
+      )
     );
   }
 }
